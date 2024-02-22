e2a9d378c1e39b55fb1ed12c2dab88488e42bd5e
Deepak Sundar Narayanan <144633279+DeepakSundarNarayanan@users.noreply.github.com>
2024-02-22 00:13:38 -0500

    Assignment 4 (#7)
    * adding validate check
    
    * checking validate
    
    * changes in yml
    
    * adding lines in readme
    
    * added readme infomation
    
    * reamed updated
    
    * reamed updated
    
    * added new lines in readme
    
    * updated readme
    
    * adding the user scripts and firewall

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = false
   routing_mode            = "REGIONAL"
   delete_default_routes_on_create = true
-
 }
 
 resource "google_compute_subnetwork" "webapp" {
@@ -30,4 +29,66 @@ resource "google_compute_route" "webapp_internet" {
   dest_range       = "0.0.0.0/0"
   network          = google_compute_network.vpc.id
   next_hop_gateway = "default-internet-gateway"
+}
+
+resource "google_compute_firewall" "allow_application_traffic" {
+  name    = "allow-application-traffic"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000","80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["webapp-vm"]
+}
+
+resource "google_compute_firewall" "allow_ssh_from_internal" {
+  name    = "allow-ssh-from-internal"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+
+  target_tags = ["webapp-vm"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image # Replace with your custom image self-link
+      type  = var.type
+      size  = var.size
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.id
+    subnetwork = google_compute_subnetwork.webapp.id
+
+    access_config {
+      // Ephemeral external IP will be assigned
+    }
+  }
+
+  tags = ["webapp-vm"]
+
+  service_account {
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  metadata_startup_script = "sudo systemctl enable csye6225.service && sudo systemctl start csye6225.service"
+}
+
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
