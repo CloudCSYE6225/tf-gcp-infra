302dd737e161aa46897c5c5a6a0878ac3f485a5f
Deepak Sundar Narayanan <144633279+DeepakSundarNarayanan@users.noreply.github.com>
2024-02-29 14:33:20 -0500

    Assignment 5 (#9)
    * adding validate check
    
    * checking validate
    
    * changes in yml
    
    * adding lines in readme
    
    * added readme infomation
    
    * reamed updated
    
    * reamed updated
    
    * added new lines in readme
    
    * updated readme
    
    * adding the user scripts and firewall
    
    * added variable
    
    * assignment_5

--- main.tf
+++ main.tf
@@ -1,94 +1,153 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "random_pet" "vpc_name_suffix" {
+  length = 2
 }
 
 resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = false
-  routing_mode            = var.routing_mode
+  name                            = var.vpc_name
+  auto_create_subnetworks         = false
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  name          = var.webapp_subnet_name
-  region        = var.region
-  network       = google_compute_network.vpc.id
+  name          = var.webapp_name
   ip_cidr_range = var.webapp_subnet_cidr
+  network       = google_compute_network.vpc.self_link
+  region        = var.region
 }
 
 resource "google_compute_subnetwork" "db" {
-  name          = var.db_subnet_name
-  region        = var.region
-  network       = google_compute_network.vpc.id
+  name          = var.db_name
   ip_cidr_range = var.db_subnet_cidr
+  network       = google_compute_network.vpc.self_link
+  region        = var.region
 }
 
-resource "google_compute_route" "webapp_internet" {
-  name             = "webapp-internet-route"
-  dest_range       = "0.0.0.0/0"
-  network          = google_compute_network.vpc.id
+resource "google_compute_global_address" "private_services_access_ip_range"{
+  provider     = google-beta
+  project      = var.project_id
+  name         = "global-psconnect-ip"
+  address_type = "INTERNAL"
+  purpose      = "VPC_PEERING" 
+  network      = google_compute_network.vpc.self_link
+  prefix_length = 16
+}
+
+resource "google_compute_route" "webapp-route" {
+  name             = var.webapp_routename
+  network          = google_compute_network.vpc.self_link
+  dest_range       = var.dest_range
+  priority         = 1000
   next_hop_gateway = "default-internet-gateway"
 }
 
-resource "google_compute_firewall" "allow_application_traffic" {
-  name    = "allow-application-traffic"
+
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.vpc.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_services_access_ip_range.name]
+}
+ 
+resource "google_compute_firewall" "allow_app_traffic" {
+  name    = var.firewall_name
   network = google_compute_network.vpc.name
 
   allow {
-    protocol = "tcp"
-    ports    = ["3000","80"]
+    protocol = var.app_protocol
+    ports    = var.app_ports
   }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["webapp-vm"]
+  source_ranges = var.source_ranges
 }
 
-resource "google_compute_firewall" "allow_ssh_from_internal" {
-  name    = "allow-ssh-from-internal"
-  network = google_compute_network.vpc.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-
-  target_tags = ["webapp-vm"]
-}
 
 resource "google_compute_instance" "vm_instance" {
-  name         = var.name
+  name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
-
+  tags         = ["http-server"]
   boot_disk {
     initialize_params {
-      image = var.image # Replace with your custom image self-link
-      type  = var.type
-      size  = var.size
+      image = var.instance_image
+      size  = var.disk_size_gb
+      type  = var.boot_disk_type
     }
   }
 
   network_interface {
-    network    = google_compute_network.vpc.id
-    subnetwork = google_compute_subnetwork.webapp.id
-
+     network    = google_compute_network.vpc.name
+     subnetwork = var.instance_subnet
     access_config {
-      // Ephemeral external IP will be assigned
+      // Ephemeral IP assigned here
+    }
+  }
+  metadata_startup_script = <<-SCRIPT
+    # Ensure /opt/webapp directory exists
+    mkdir -p /opt/webapp
+
+    echo "working 1"
+
+    CLOUD_SQL_PRIVATE_IP=$(gcloud sql instances describe ${google_sql_database_instance.cloudsql_instance.name} --format="get(ipAddresses[?type=PRIVATE].ipAddress)")
+    echo "working 2"
+    # Write the .env file
+    echo "MYSQL_USER=${google_sql_user.webapp_user.name}" > /opt/webapp/.env
+    echo "MYSQL_PASSWORD=${random_password.password.result}" >> /opt/webapp/.env
+    echo "MYSQL_HOST=${google_sql_database_instance.cloudsql_instance.private_ip_address}" >> /opt/webapp/.env
+    echo "MYSQL_PORT=3306" >> /opt/webapp/.env
+    echo "MYSQL_DATABASE=${google_sql_database.webapp.name}" >> /opt/webapp/.env
+    echo "working 3"
+
+    # Enable and start the csye6225.service 
+    sudo systemctl daemon-reload
+    sudo systemctl restart csye6225.service
+    sudo systemctl enable csye6225.service
+    echo "working 4"
+  SCRIPT
+}
+
+resource "google_sql_database_instance" "cloudsql_instance" {
+  provider = google-beta
+
+  name                = "instance"
+  project             = var.project_id
+  region              = var.region
+  database_version    = "MYSQL_8_0"
+  deletion_protection = false
+  depends_on = [google_service_networking_connection.default]  
+  settings {
+    tier                        = "db-custom-1-3840"
+    activation_policy           = "ALWAYS"
+    availability_type           = "REGIONAL"
+    disk_size                   = 100
+    disk_type                   = "pd-ssd"
+    ip_configuration {
+      ipv4_enabled              = false
+      private_network           = google_compute_network.vpc.id
+    }
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true 
     }
   }
 
-  tags = ["webapp-vm"]
-
-  service_account {
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  metadata_startup_script = "sudo systemctl enable csye6225.service && sudo systemctl start csye6225.service"
 }
 
-output "instance_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+resource "google_sql_database" "webapp"{
+  name = var.database_name
+  instance = google_sql_database_instance.cloudsql_instance.name
+}
+
+resource "random_password" "password" {
+  length  = 16
+  special = false
+}
+
+resource "google_sql_user" "webapp_user" {
+  name     = var.user_name
+  instance = google_sql_database_instance.cloudsql_instance.name
+  password = random_password.password.result
 }
